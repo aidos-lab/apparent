a766ee575782ced615a4528107a7455a39f69cdd
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-02-21 07:53:23 +0100

    relative path

--- doc_orc/config.py
+++ doc_orc/config.py
@@ -1,2 +1,1 @@
-CONNECTION_STRING = "mysql+pymysql://root:russell0@localhost"
-DATA_PATH = "/Users/jeremy.wayland/Desktop/Dev/Nammu/patient_networks/network_construction_20230213/"
+DATA_PATH = "../data/"
