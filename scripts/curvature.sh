e636458ab144d2bd7c7fbdcd46c9a341e6368e05
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-03-10 14:22:36 +0100

    curvature script stub

---
+++ scripts/curvature.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -o ./log_files/curvature_output.txt
+#SBATCH -e ./log_files/curvature_error.txt
+#SBATCH -J Wayland_doc-orc_curvature
+#SBATCH -p cpu_p
+#SBATCH -c 1
+#SBATCH --mem=30G
+#SBATCH -t 01:00:00
+#SBATCH --nice=10000 
+
+# You can put arbitrary unix commands here, call other scripts, etc...
+
+DATA=../data/test_sample.pkl
+alpha=0
+
+poetry run python ../doc_orc/analysis.py --data ${DATA} --alpha ${alpha}
+echo "Finished Generating Networks!"
