49799f9e21efb0d309f4a88657ba39da7c6e0344
jeremy-wayland <jeremy.don.wayland@gmail.com>
2025-07-22 14:32:28 +0200

    Add validation for required columns in query results

--- apparent/apparent.py
+++ apparent/apparent.py
@@ -128,6 +128,13 @@ class Apparent:
         if os.path.isfile(sql_query):
             sql_query = self._read_query(sql_query)
         self.data = self._fetcher(sql_query)
+        
+        # Validate required columns exist
+        required_columns = ["hsa", "year"]
+        missing_columns = [col for col in required_columns if col not in self.data.columns]
+        if missing_columns:
+            raise ValueError(f"Query results must contain 'hsa' and 'year' columns. Missing: {missing_columns}")
+        
         self.data.sort_values(["hsa", "year"], inplace=True)
 
         self.network_ids = list(
